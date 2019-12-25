b0f49dd00fecb8d30ec1f2f3a417b6f206ef4b2f
yaylinda <lindazheng1993@gmail.com>
2019-12-25 12:59:07 -0600

    add basic buttons/modal

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,111 +1,55 @@
-import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Image,
   Platform,
-  ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
 } from 'react-native';
-
-import { MonoText } from '../components/StyledText';
+import {
+  Button,
+  Input,
+  Overlay
+} from 'react-native-elements';
 
 export default function HomeScreen() {
+
+  const [showAddActivityModal, setShowAddActivityModal] = useState(false);
+
   return (
     <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
+
+      <Button title="Add Activity" onPress={() => handlePressedAddActivity(setShowAddActivityModal)} />
+
+      <Overlay isVisible={showAddActivityModal}>
+
+        <View>
+          <Text>New Activity</Text>
+          <Input placeholder="Enter Activity" />
+          <Button title="Cancel" onPress={() => handleCancelAddActivity(setShowAddActivityModal)} />
+
+          <Button title="Add" onPress={() => handleSaveActivity(setShowAddActivityModal)} />
+
         </View>
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
+      </Overlay>
     </View>
   );
 }
 
 HomeScreen.navigationOptions = {
-  header: null,
+  title: 'My Day',
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
+function handlePressedAddActivity(setShowAddActivityModal) {
+  setShowAddActivityModal(true);
 }
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
+function handleCancelAddActivity(setShowAddActivityModal) {
+  setShowAddActivityModal(false);
 }
 
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
+function handleSaveActivity(setShowAddActivityModal) {
+  setShowAddActivityModal(false);
 }
 
 const styles = StyleSheet.create({
